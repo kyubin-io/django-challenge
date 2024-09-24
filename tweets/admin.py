6384adf48ba649e871f5a8285c68317a93f73fe2
김규빈 <inkblot626@gmail.com>
2024-09-24 21:30:41 +0900

    related name and count()

--- tweets/admin.py
+++ tweets/admin.py
@@ -4,14 +4,14 @@ from .models import Tweet, Like
 
 @admin.register(Tweet)
 class TweetAdmin(admin.ModelAdmin):
-    list_display = (
-        "__str__",
-    )
 
+    list_display = (
+        "payload",
+        "user",
+        "like_count",
+    )
 
 
 @admin.register(Like)
 class LikeAdmin(admin.ModelAdmin):
-    list_display = (
-        "__str__",
-    )
+    pass
--- tweets/models.py
+++ tweets/models.py
@@ -6,17 +6,32 @@ class Tweet(CommonModel):
     """ Tweet Model Definition """
 
     payload = models.CharField(max_length=180)
-    user = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name="tweets")
+    user = models.ForeignKey(
+        'users.User',
+        on_delete=models.CASCADE,
+        related_name='tweets',
+    )
 
     def __str__(self):
         return self.payload
+
+    def like_count(self):
+        return self.likes.count()
 
 
 class Like(CommonModel):
     """ Like Model Definition """
 
-    user = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name="like")
-    tweet = models.ForeignKey('tweets.Tweet', on_delete=models.CASCADE, related_name="like")
+    user = models.ForeignKey(
+        'users.User',
+        on_delete=models.CASCADE,
+        related_name='likes',
+    )
+    tweet = models.ForeignKey(
+        'tweets.Tweet',
+        on_delete=models.CASCADE,
+        related_name='likes',
+    )
 
     def __str__(self):
         return f"{self.tweet.payload}"
