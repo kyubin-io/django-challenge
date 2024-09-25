529864d141b32d214d3ea3985b9fe7abfaee7d9e
김규빈 <inkblot626@gmail.com>
2024-09-26 02:05:48 +0900

    filter by elon

--- tweets/admin.py
+++ tweets/admin.py
@@ -2,25 +2,24 @@ from django.contrib import admin
 from .models import Tweet, Like
 
 
-class WordFilter(admin.SimpleListFilter):
+class ElonFilter(admin.SimpleListFilter):
 
-    title = "Filter by Elon Musk"
+    title = "Filter by like count"
 
-    parameter_name = "word"
+    parameter_name = "likes"
 
     def lookups(self, request, model_admin):
         return [
-            ("elon musk", "Elon Musk"),
+            ("elon", "Tweets with Elon"),
+            ("no_elon", "Tweets without Elon"),
         ]
-    
+
     def queryset(self, request, tweets):
-        word = self.value()
-        if word:
-            return tweets.filter(payload__contains=word)
-        else:
-            tweets
-
-
+        likes = self.value()
+        if likes == "elon":
+            return tweets.filter(payload__contains='Elon Musk')
+        elif likes == "no_elon":
+            return tweets.exclude(payload__contains='Elon Musk')
 
 
 @admin.register(Tweet)
@@ -33,26 +32,19 @@ class TweetAdmin(admin.ModelAdmin):
     )
 
     search_fields = (
-        "payload",
-        "user__username",
+      "payload",
+      "user__username",
     )
 
     list_filter = (
-        WordFilter,
-        "created_at",
+      ElonFilter,
+      "created_at",
     )
-
-
 
 
 @admin.register(Like)
 class LikeAdmin(admin.ModelAdmin):
 
-    search_fields = (
-        "user__username",
-    )
+    search_fields = ("user__username", )
 
-    list_filter = (
-        "created_at",
-    )
-
+    list_filter = ("created_at", )
