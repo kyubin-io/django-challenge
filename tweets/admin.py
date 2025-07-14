1c429c04cce17f42938f94a7efdbc4eb8e31f940
김규빈 <inkblot626@gmail.com>
2025-07-15 01:23:54 +0900

    05 solution

--- tweets/admin.py
+++ tweets/admin.py
@@ -1,20 +1,12 @@
 from django.contrib import admin
 from .models import Tweet, Like
 
+
 @admin.register(Tweet)
 class TweetAdmin(admin.ModelAdmin):
-    list_display = (
-        "payload",
-        "user",
-        "created_at",
-        "updated_at"
-    )
+    pass
+
 
 @admin.register(Like)
 class LikeAdmin(admin.ModelAdmin):
-        list_display = (
-        "user",
-        "tweet",
-        "created_at",
-        "updated_at"
-    )
+    pass
