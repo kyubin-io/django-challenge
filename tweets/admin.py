ce0675f0aad20bf06a187dcf43201d51d461b039
김규빈 <inkblot626@gmail.com>
2025-07-16 00:42:23 +0900

    add search field

--- tweets/admin.py
+++ tweets/admin.py
@@ -6,7 +6,11 @@ from .models import Tweet, Like
 class TweetAdmin(admin.ModelAdmin):
     list_display = ("payload", "user", "like_count", "created_at", "updated_at", )
 
+    search_fields = ("payload", "user__name", )
+
 
 @admin.register(Like)
 class LikeAdmin(admin.ModelAdmin):
     list_display = ("user", "tweet",  )
+
+    search_fields = ("user__name", )
