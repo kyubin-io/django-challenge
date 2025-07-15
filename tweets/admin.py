dafa43846e2b79b034c6c21ffb6075dd29737d18
김규빈 <inkblot626@gmail.com>
2025-07-16 01:13:15 +0900

    create custom filter

--- tweets/admin.py
+++ tweets/admin.py
@@ -2,11 +2,36 @@ from django.contrib import admin
 from .models import Tweet, Like
 
 
+class elonMusk(admin.SimpleListFilter):
+
+    title = "Filter by Elon Musk"
+
+    parameter_name = "musk"
+
+    def lookups(self, request, model_admin):
+        return [
+            ("musk", "Elon Musk"),
+            ("no_musk", "No Musk"),
+        ]
+    
+    def queryset(self, request, tweets):
+        musk = self.value()
+        if musk == "musk":
+            return tweets.filter(payload__icontains="Elon Musk")
+        elif musk == "no_musk":
+            tweets.exclude(payload__icontains="Elon Musk")
+        else:
+            tweets
+
+
+
 @admin.register(Tweet)
 class TweetAdmin(admin.ModelAdmin):
     list_display = ("payload", "user", "like_count", "created_at", "updated_at", )
 
     search_fields = ("payload", "user__name", )
+
+    list_filter = ("created_at", )
 
 
 @admin.register(Like)
@@ -14,3 +39,5 @@ class LikeAdmin(admin.ModelAdmin):
     list_display = ("user", "tweet",  )
 
     search_fields = ("user__name", )
+
+    list_filter = ("created_at", )
